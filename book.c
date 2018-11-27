f50b74befb2e2a036a71903b0f5100da0a14e8ce
Vivian Weaver <veweaver@cougar.cs.ksu.edu>
2018-11-27 14:13:54 -0600

    more initial files

---
+++ book.c
@@ -0,0 +1,13 @@
+//
+// book.c
+// 308 Final Project
+// 
+// Created by Emily Davich and Vivian Weaver
+// Copyright 2018
+//
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "book.h"
+
---
+++ book.h
@@ -0,0 +1,14 @@
+//
+// book.h
+// 308 Final Project
+// 
+// Created by Emily Davich and Vivian Weaver
+// Copyright 2018
+//
+
+#ifndef book_h
+#define book_h
+
+int something(int argc, const char * argv[]);
+
+#endif /* book.h */
