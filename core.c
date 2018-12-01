6e25a2e7430da0557514c1c790474bd7ed598937
Vivian Weaver <veweaver@cougar.cs.ksu.edu>
2018-12-01 14:54:12 -0600

    p1

--- core.c
+++ core.c
@@ -13,6 +13,11 @@
 #include "core.h"
 
 int core_main(int argc, const char * argv[]) {
-    printf("%s", "/t/tWELCOME TO BOOKS");
+    printf("%s", "\n\n\t\tWELCOME TO BOOKS\n\n");
+    printf("How would you like to search the library?");
+    printf("\nTitle(T), Author(A), Date(D), List All (L):   ");
+    char choice = getchar();
+    printf("\nYour choice is %c", choice);
+    printf("%s", "\n\n\n");
     return 0;
 }
