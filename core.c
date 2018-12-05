81e8d7d3bf3fbad6c99c2083acc19d92885c618a
Vivian Weaver <veweaver@viper.cs.ksu.edu>
2018-12-04 20:25:40 -0600

    polished

--- core.c
+++ core.c
@@ -19,17 +19,17 @@
 //Search through library of books  by title
 //c = user's choice
 
-
+//search directory method
 Library* search(const char c, Library * cur){
     Library * result = (Library *)malloc(sizeof(Library));
     if (cur == NULL) return NULL;
+    //search by title
     if (c == 'T'){
         char name[50];
         printf("Enter title:   ");
         fgets(name,50,stdin); 
         name[strlen(name)-1] = '\0'; 
 	while(cur != NULL){
-	    //printf("%s,%s", cur->booklist.title, name);
             if (strstr(cur->booklist.title, name)!=NULL){
                 strcpy(result->booklist.title, cur->booklist.title);
 		result->next = NULL;
@@ -38,7 +38,7 @@ Library* search(const char c, Library * cur){
             cur = cur->next;
     	}
     }
-        //Search through library of books by author 
+    //search by author
     else if (c=='A'){
         char name[50];
         printf("Enter author:   ");
@@ -53,13 +53,13 @@ Library* search(const char c, Library * cur){
 	    cur = cur->next;
         }
     }
+    //search by date
      else if (c=='D'){
         char name[10];
         printf("Enter publication date:   ");
         fgets(name, 10, stdin);
 	name[strlen(name)-1] = '\0';
         int date = atoi(name);
-	printf("%d\n", date);
         while(cur != NULL){
             if (cur->booklist.date == date){
                 strcpy(result->booklist.title, cur->booklist.title);
@@ -69,9 +69,11 @@ Library* search(const char c, Library * cur){
 	    cur = cur->next;
         }
     }
+    //search by all
     else if (c=='L'){
         return cur;
     }
+    //if bad input
     else{
         return NULL;
     }  
@@ -86,7 +88,8 @@ int core_main(int argc, const char * argv[]) {
     Library * curLib = NULL;
     Shelf * shelf = malloc(sizeof(Shelf));
     shelf->total = 0;
-
+    int stay = 1;
+while(stay) { 
     char choice[5];
     int resultNotFound = 1;
 
@@ -122,7 +125,6 @@ int core_main(int argc, const char * argv[]) {
 	       curLib = curLib->next;
             }
             
-           // printf("%s", buf);
 	    
             //tokenize string
 	    char* token = strtok(buf, ",");
@@ -138,11 +140,12 @@ int core_main(int argc, const char * argv[]) {
 	    token = strtok(NULL, ",");
 	    token[strlen(token)-2] = '\0';
             strcpy(curLib->booklist.filename, token);
-        //    printf("%s", curLib->booklist.filename);
 	}
+
 	curLib->next = NULL;
 	curLib = lib;
 
+        //search for books call and print
         Library* result = search(*choice, curLib);
 	if (result != NULL) {
 	    printf("\n\tBooks found: \n");
@@ -157,35 +160,21 @@ int core_main(int argc, const char * argv[]) {
 	}
 	//close the file
 	fclose(dir);
-	/*
-	curLib = lib;
-        strcpy(curLib->booklist.filename, "Books/AliceInWonderland.txt");
-	curLib = curLib->next;   
-	strcpy(curLib->booklist.filename, "Books/Dracula.txt");
-	curLib = curLib->next;    
-	strcpy(curLib->booklist.filename, "Books/Grimm'sFairyTales.txt");
-	curLib = curLib->next;    
-	strcpy(curLib->booklist.filename, "Books/MobyDick.txt");
-	curLib = curLib->next;     
-	strcpy(curLib->booklist.filename, "Books/PrideAndPrejudice.txt");
-*/
     }
     
-    //printf("%s", curLib->booklist.filename);
  
     char selectedTitle[50];
     printf("\nSelect a title to read:   ");
     fgets(selectedTitle,50,stdin);
     selectedTitle[strlen(selectedTitle)-1] = '\0';
     
+    //prepare selected book
     Book selectedBook;
     curLib = lib;
     while(curLib != NULL){
-        if (strstr(curLib->booklist.title, selectedTitle) != NULL){
-	    //selectedBook = curLib->booklist;
+        if (strstr(curLib->booklist.title, selectedTitle) != NULL){ 
             memcpy(&selectedBook, &(curLib->booklist), sizeof(Book));
             strcpy(selectedBook.filename, curLib->booklist.filename);
-            //printf("%s", selectedBook.filename);
 	}
 	curLib = curLib->next;
     }
@@ -204,50 +193,50 @@ int core_main(int argc, const char * argv[]) {
     if(* favorite == 'Y'){
         print_shelf(shelf);
 	printf("Continue(C):   ");
-	char wait;
-	scanf(" %c", &wait);
+	char wait[5];
+	fgets(wait,5,stdin);
     }
 
-    //print out the book
+    //print out the book, using file IO
     
     char path[100];
     strcpy(path, "Books/");
     strcat(path, selectedBook.filename);
-    //printf("%s", selectedBook.filename);
-    //strcat(path, "\0"); 
 
     printf("\n\n\nBook Title: <%s>\n", selectedBook.title);
     FILE *fp;
     fp = fopen(path, "r");
 
     //navigate through book
-    char nav = 'N';
-    char ch;
+    char nav[5];
+    char ch[5];
     
     printf("\nTo navigate select Next(N) or Exit(E):   ");
-    scanf(" %c", &ch);
-    nav = ch;
+    fgets(ch,5,stdin);
+    strcpy(nav, ch);
 
-	if (nav == 'E'){
-	    printf("\n\n\t\tThank you for choosing Books! \n");
+	if (*nav == 'E'){
 	    printf("\n");
-	    return 0;
     }
-    while(nav == 'N'){ 
-	if (nav == 'E'){
-	    printf("\n\n\t\tThank you for choosing Books! \n");
+    while(*nav == 'N'){ 
+	if (*nav == 'E'){
 	    printf("\n");
 	    break;
 	}
 	read_book(&fp, &selectedBook);
 
 	printf("\nTo navigate select Next(N) or Exit(E):   ");
-        scanf(" %c", &ch);
-	nav = ch;
+        fgets(ch,5,stdin);
+	strcpy(nav, ch);
     }
-    //fclose(fp);
-    //printf("%s\n", loadedBook->content.pages[0]);
-
+   
+    //ask to continue or leave
+    char toContinue[5];
+    printf("\nWould you like to Search(S) again or Leave(L) books:   ");
+    fgets(toContinue,5,stdin);
+    if (*toContinue == 'L') stay = 0;
+}
+    printf("\n\n\t\tThank you for choosing Books! \n");
     return 0;
 }
 
